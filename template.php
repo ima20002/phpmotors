620b49ac60269321fa18896ab0ac8c5a3701c4ec
ima20002 <77100505+ima20002@users.noreply.github.com>
2021-09-25 16:14:45 +0900

    commit

---
+++ template.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP Motors</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" media="screen" href="/phpmotors/css/motor.css" type="text/css">
+    </head>
+    <body>
+        <div id="wrapper">
+            <header>
+                <div id="to-header">
+                    <img src="/phpmotors/images/site/logo.png" alt="Logo">
+                    <a href="">My Account</a>
+                </div>
+            </header>
+            <nav>
+                    <ul>
+                        <li><a href="">Home</a></li> 
+                        <li class="active"><a href="">Classic</a></li>
+                        <li><a href="">Sports</a></li>
+                        <li><a href="">SUV</a></li>
+                        <li><a href="">Trucks</a></li>
+                        <li><a href="">Used</a></li>
+                    </ul>
+            </nav>
+            <main>
+                <div class="main_content">
+                    <h1>Content Title Here</h1>
+            </main>
+            <hr>
+            <footer>
+                <p>&copy; PHP Motors, All rights reserved.</p>
+                <p>All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.</p>
+                <p>Last Updated: 30 March, 2018</p>
+            </footer>
+        </div>
+        <script src="../js/inventory.js"></script>
+    </body>
+</html>
